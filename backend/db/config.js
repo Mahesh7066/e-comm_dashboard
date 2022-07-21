329015248987b16ca65164bd7822627e54143384
Mahesh7066 <maheshchand171999@gmail.com>
2022-07-22 00:52:00 +0530

    first commit

---
+++ backend/db/config.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/e-comm')
+mongoose.connect('mongodb+srv://Sarita123:obhezGAuHCz7ltkT@saritacluster.9jdhy.mongodb.net/e-comm?retryWrites=true&w=majority')
+
+
+
+// password:     obhezGAuHCz7ltkT
